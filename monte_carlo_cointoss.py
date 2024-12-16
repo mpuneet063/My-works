4fbd0c551d04f12874cd72dc6325213acdd4afc8
mpuneet063 <167070714+mpuneet063@users.noreply.github.com>
2024-12-16 10:09:18 +0530

    add work files

---
+++ monte_carlo_cointoss.py
@@ -0,0 +1,11 @@
+from random import randint
+
+success = 0
+attempts = 100000
+
+for i in range(attempts):
+    if randint(0,1) + randint(0,1) + randint(0,1) == 3:
+        success += 1
+
+print("Number of attempts: " , attempts)
+print("Number of successes: " , success)
